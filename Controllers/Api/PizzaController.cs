14d1abbd1f9916626d5575c07d6e4f6f8d7fcf30
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-26 14:15:13 +0200

    creata Api PizzaController Get

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,23 @@
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/pizze")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public IActionResult Get()
+        {
+            PizzaContext context = new PizzaContext();
+
+            IQueryable<Pizza> listaPizze = context.Pizze;
+
+            return Ok(listaPizze);
+        }
+    }
+}
