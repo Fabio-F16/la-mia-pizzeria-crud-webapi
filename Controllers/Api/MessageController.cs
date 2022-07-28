d39e966c6d4a388ac2ad424a5ed173804d1c4f01
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-28 16:36:33 +0200

    MessageController

---
+++ Controllers/Api/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/message")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+
+        [HttpPost]
+      public IActionResult Post( Message message)
+        {
+
+            try
+            {
+                using (PizzaContext context = new PizzaContext())
+                {
+                    context.Messages.Add(message);
+                    context.SaveChanges();
+                }
+                return Ok(new {Status = "ok", Message = "Dati corretti"});
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+           
+        }
+       
+    }
+}
+
